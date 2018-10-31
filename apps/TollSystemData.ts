305fb5d26acfde7a84bbdfff833e0869e020d60a
al <anna.lickl@loremipsum.at>
2018-10-31 13:26:31 +0100

    add TollSystem Example data

---
+++ apps/TollSystemData.ts
@@ -0,0 +1,28 @@
+import {App} from "../src/Decorator/App";
+import {Route} from "../src/Decorator/Route";
+import {HttpMethod} from "../src/Enum/HttpMethod";
+import {JsonResponse} from "../src/Response/JsonResponse";
+
+@App({
+    name: 'TollSystemData',
+    port: 62000
+})
+export class TollSystemData {
+    @Route("/tollsystem/list", HttpMethod.GET)
+    getTollSystems() {
+        return new JsonResponse({
+            "ts-1": {
+                "name": "Toll System 1",
+                "status": "ok",
+                "lastStateChange": '12:34:56'
+            }
+            ,
+            "ts-2": {
+                "name": "Toll System 2",
+                "status": "warning",
+                "lastStateChange": '12:34:56'
+            }
+
+        })
+    }
+}
