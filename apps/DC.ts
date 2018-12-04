3d2cf2fffcb83c7223880c75d6138710b43b4923
al <anna.lickl@loremipsum.at>
2018-12-04 14:43:55 +0100

    add transaction files

--- apps/DC.ts
+++ apps/DC.ts
@@ -1,5 +1,6 @@
 import {App} from "../src/Decorator/App";
 import {Websocket} from "../src/Decorator/Websocket";
+import {JsonResponse} from "../src/Response/JsonResponse";
 
 @App({
     name: 'DC',
@@ -147,6 +148,40 @@ export class DC {
         console.log(`wait 1s...`);
         await new Promise(resolve => setTimeout(resolve, 1000));
         ws.send(transaction, function ack(error) {
+            error && console.error(error);
+        });
+    }
+
+    /**
+     * Response of files data
+     */
+    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/files-uuid')
+    async onTransactionFileResponse(ws) {
+        let files = new JsonResponse({
+            "files": [
+                {
+                    "filename": "file1",
+                    "created": "2001-12-17T09:30:47Z",
+                    "status": "transferred",
+                    "time": "2001-12-17T09:30:47Z",
+                    "history": [{"status": "queued", "time": "2001-12-17T09:30:47Z"}]
+                },
+                {
+                    "filename": "file2",
+                    "created": "2001-12-17T09:30:47Z",
+                    "status": "queued",
+                    "time": "2001-12-17T09:30:47Z",
+                    "history": [{"status": "transferred", "time": "2001-12-17T09:30:47Z"}]
+                }
+            ],
+            "status": "ok",
+            "message": "message1",
+        });
+
+        // wait 1s to allow onTransactionFilesRequest connection
+        console.log(`wait 1s...`);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        ws.send(JSON.stringify(files), function ack(error) {
             error && console.error(error);
         });
     }
