155352dafbad39d882a6d08317ac60d7f857e713
kh <kh@loremipsum.at>
2019-02-20 16:04:12 +0100

    Add main state and spot test mode endpoints

--- apps/DC.ts
+++ apps/DC.ts
@@ -1,117 +1,202 @@
 import {App} from "../src/Decorator/App";
-import {Websocket} from "../src/Decorator/Websocket";
 import {JsonResponse} from "../src/Response/JsonResponse";
-import * as fs from "fs";
+import {Route} from "../src/Decorator/Route";
+import {HttpMethod} from "../src/Enum/HttpMethod";
 
 @App({
     name: 'DC',
     port: 65000
 })
 export class DC {
-    /**
-     * On connection to the global room.
-     */
-    @Websocket('connection')
-    onGlobalTopicConnection() {
-        console.log(`Someone connected to /. Nothing to see here...`);
-    }
-
-    /**
-     * Request of transaction data
-     */
-    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/transactions/get')
-    onTransactionDataRequest(ws) {
-        ws.on('message', function (message) {
-            console.log(`Incoming message: ${message}`);
+    @Route("/1.0.0/cp/:cpid/:cpsystem/current_state", HttpMethod.GET)
+    public getChargingPointMainState(req) {
+        this.validateRequest(req);
+        return new JsonResponse({
+            state: this.getRandomChargingPointState(),
+            timestamp: this.generateTimestamp()
         });
     }
 
-    /**
-     * Response of transaction data
-     */
-    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/transactions-uuid')
-    async onTransactionDataResponse(ws) {
-        // wait 1s to allow onTransactionDataRequest connection
-        let transactions = fs.readFileSync("public/file/transactions.txt");
-        console.log(`wait 1s...`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        ws.send(transactions, function ack(error) {
-            error && console.error(error);
+    @Route("/1.0.0/cp/:cpid/:cpsystem/target_state", HttpMethod.POST)
+    public setChargingPointMainState() {
+        return new JsonResponse({
+            source: 'mcs',
+            primary_state: this.getRandomChargingPointState(),
+            secondary_state: this.getRandomChargingPointState(),
+            begin: this.generateTimestamp()
         });
     }
 
-    /**
-     * Response of files data
-     */
-    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/files-uuid')
-    async onTransactionFileResponse(ws) {
-        let files = new JsonResponse({
-            "files": [
-                {
-                    "filename": "file1",
-                    "created": "2001-12-17T09:30:47Z",
-                    "status": "transferred",
-                    "time": "2001-12-17T09:30:47Z",
-                    "history": [{"status": "queued", "time": "2001-12-17T09:30:47Z"}]
-                },
-                {
-                    "filename": "file2",
-                    "created": "2001-12-17T09:30:47Z",
-                    "status": "queued",
-                    "time": "2001-12-17T09:30:47Z",
-                    "history": [{"status": "transferred", "time": "2001-12-17T09:30:47Z"}]
-                }
-            ],
-            "status": "ok",
-            "message": "message1",
-        });
-
-        // wait 1s to allow onTransactionFilesRequest connection
-        console.log(`wait 1s...`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        ws.send(JSON.stringify(files), function ack(error) {
-            error && console.error(error);
+    @Route("/1.0.0/cp/:cpid/:cpsystem/current_spottest", HttpMethod.GET)
+    public getSpotTestModeConfiguration() {
+        return new JsonResponse({
+            'ison': 'true',
+            'begin': this.generateTimestamp(),
+            'end': this.generateTimestamp(),
+            'timestamp': this.generateTimestamp(),
+            'config': {
+                'transaction': this.getRandomTransaction(),
+                'isenvironmental': this.getRandomBooleanValue(),
+                'missmatchedlpn': this.getRandomBooleanValue(),
+                'hastrailer': this.getRandomBooleanValue(),
+                'tagstatusflag': this.getRandomStatusFlag(),
+                'signalcode': this.getRandomSignalCode(),
+                'classes': this.getRandomClass(),
+                'quantity': 42
+            }
         });
     }
 
-    /**
-     * Response of download files data
-     */
-    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/download-uuid')
-    async onTransactionFileDownloadResponse(ws) {
-        let files = new JsonResponse('file.tar.gz');
+    @Route("/1.0.0/cp/:cpid/:cpsystem/target_spottest", HttpMethod.POST)
+    public setSpotTestMode() {
+        return this.getSpotTestModeConfiguration();
+    }
 
-        // wait 1s to allow onTransactionFilesRequest connection
-        console.log(`wait 1s...`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        ws.send(JSON.stringify(files), function ack(error) {
-            error && console.error(error);
-        });
+    private validateRequest(req) {
+        if (req.params.cpsystem && ['p', 's'].indexOf(req.params.cpsystem) < 0) {
+            throw new Error('Invalid cpsystem (allowed are either "p" or "s")');
+        }
+    }
+
+    private getRandomClass() {
+        return this.getRandomValueFromArray(['car', 'van', 'truck', 'bus', 'motorcycle']);
+    }
+
+    private getRandomSignalCode() {
+        return this.getRandomValueFromArray(['02', '08', '10', '20', '22', '23', '25', '28', '40', '41', '42']);
+    }
+
+    private getRandomTransaction() {
+        return this.getRandomValueFromArray(['EN15509', 'AutoPASS']);
+    }
+
+    private getRandomStatusFlag() {
+        return this.getRandomValueFromArray(['wanted', 'video']);
+    }
+
+    private getRandomBooleanValue() {
+        return this.getRandomValueFromArray(['true', 'false']);
+    }
+
+    private getRandomChargingPointState() {
+        return this.getRandomValueFromArray(['charging', 'standby']);
+    }
+
+    private getRandomValueFromArray(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
 
     /**
-     * Response of retransfer files data
+     * Returns a RFC3339 formatted date
      */
-    @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/retransfer-uuid')
-    async onTransactionFileRetransferResponse(ws) {
-        let files = new JsonResponse({
-            "files": [
-                {
-                    "filename": "file1",
-                    "created": "2001-12-17T09:30:47Z",
-                    "status": "transferred",
-                    "time": "2001-12-17T09:30:47Z",
-                    "history": [{"status": "queued", "time": "2001-12-17T09:30:47Z"}]
-                }],
-            "status": "ok",
-            "message": "message: transferred",
-        });
-
-        // wait 1s to allow onTransactionFilesRequest connection
-        console.log(`wait 1s...`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        ws.send(JSON.stringify(files), function ack(error) {
-            error && console.error(error);
-        });
+    private generateTimestamp() {
+        return (new Date()).toISOString();
     }
+
+    // /**
+    //  * On connection to the global room.
+    //  */
+    // @Websocket('connection')
+    // onGlobalTopicConnection() {
+    //     console.log(`Someone connected to /. Nothing to see here...`);
+    // }
+    //
+    // /**
+    //  * Request of transaction data
+    //  */
+    // @Websocket('connection', '/efkon/no/dc/ctrl/cmd/transactions/get')
+    // onTransactionDataRequest(ws) {
+    //     ws.on('message', function (message) {
+    //         console.log(`Incoming message: ${message}`);
+    //     });
+    // }
+    //
+    // /**
+    //  * Response of transaction data
+    //  */
+    // @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/transactions-uuid')
+    // async onTransactionDataResponse(ws) {
+    //     // wait 1s to allow onTransactionDataRequest connection
+    //     let transactions = fs.readFileSync("public/file/transactions.txt");
+    //     console.log(`wait 1s...`);
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //     ws.send(transactions, function ack(error) {
+    //         error && console.error(error);
+    //     });
+    // }
+    //
+    // /**
+    //  * Response of files data
+    //  */
+    // @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/files-uuid')
+    // async onTransactionFileResponse(ws) {
+    //     let files = new JsonResponse({
+    //         "files": [
+    //             {
+    //                 "filename": "file1",
+    //                 "created": "2001-12-17T09:30:47Z",
+    //                 "status": "transferred",
+    //                 "time": "2001-12-17T09:30:47Z",
+    //                 "history": [{"status": "queued", "time": "2001-12-17T09:30:47Z"}]
+    //             },
+    //             {
+    //                 "filename": "file2",
+    //                 "created": "2001-12-17T09:30:47Z",
+    //                 "status": "queued",
+    //                 "time": "2001-12-17T09:30:47Z",
+    //                 "history": [{"status": "transferred", "time": "2001-12-17T09:30:47Z"}]
+    //             }
+    //         ],
+    //         "status": "ok",
+    //         "message": "message1",
+    //     });
+    //
+    //     // wait 1s to allow onTransactionFilesRequest connection
+    //     console.log(`wait 1s...`);
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //     ws.send(JSON.stringify(files), function ack(error) {
+    //         error && console.error(error);
+    //     });
+    // }
+    //
+    // /**
+    //  * Response of download files data
+    //  */
+    // @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/download-uuid')
+    // async onTransactionFileDownloadResponse(ws) {
+    //     let files = new JsonResponse('file.tar.gz');
+    //
+    //     // wait 1s to allow onTransactionFilesRequest connection
+    //     console.log(`wait 1s...`);
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //     ws.send(JSON.stringify(files), function ack(error) {
+    //         error && console.error(error);
+    //     });
+    // }
+    //
+    // /**
+    //  * Response of retransfer files data
+    //  */
+    // @Websocket('connection', '/efkon/no/dc/ctrl/cmd/response/retransfer-uuid')
+    // async onTransactionFileRetransferResponse(ws) {
+    //     let files = new JsonResponse({
+    //         "files": [
+    //             {
+    //                 "filename": "file1",
+    //                 "created": "2001-12-17T09:30:47Z",
+    //                 "status": "transferred",
+    //                 "time": "2001-12-17T09:30:47Z",
+    //                 "history": [{"status": "queued", "time": "2001-12-17T09:30:47Z"}]
+    //             }],
+    //         "status": "ok",
+    //         "message": "message: transferred",
+    //     });
+    //
+    //     // wait 1s to allow onTransactionFilesRequest connection
+    //     console.log(`wait 1s...`);
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //     ws.send(JSON.stringify(files), function ack(error) {
+    //         error && console.error(error);
+    //     });
+    // }
 }
