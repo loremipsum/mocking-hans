1c33d640ae0ebb86c2728ce795f83a8186483102
Daniel <dl@loremipsum.at>
2019-02-22 19:46:46 +0100

    Revert "Merge branch 'control-view' into 'master'"
    This reverts merge request !2

--- index.ts
+++ index.ts
@@ -2,18 +2,12 @@ import "reflect-metadata";
 import {Example} from "./apps/Example";
 import {Twitter} from "./apps/Twitter";
 import {Hans} from "./src/Hans";
-import {Grafana} from "./apps/Grafana";
-import {Icinga} from "./apps/Icinga";
-import {DC} from "./apps/DC";
 
 const chalk = require('chalk');
 
 const apps = [
     Example,
-    Twitter,
-    Grafana,
-    Icinga,
-    DC
+    Twitter
 ];
 
 (new Hans(apps)).bootstrap().then(() => {
--- src/Decorator/Websocket.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-import {WebsocketRegistry} from "../Registry/WebsocketRegistry";
-
-export const Websocket = (event: string, namespace: string = '/'): MethodDecorator => {
-    return (target: any, propertyKey: string): void => {
-        WebsocketRegistry.add(target, propertyKey, event, namespace);
-    };
-};
