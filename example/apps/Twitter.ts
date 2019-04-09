05dc63b00faf0dc2f408dd99b8f3ee10bf4047b3
kh <kh@loremipsum.at>
2019-04-09 09:50:43 +0200

    Fix coding style

--- example/apps/Twitter.ts
+++ example/apps/Twitter.ts
@@ -2,22 +2,22 @@ import {App, Get} from '@loremipsum/mocking-hans/decorators';
 import {JsonResponse} from '@loremipsum/mocking-hans/response';
 
 @App({
-    name: 'twitter',
-    port: 61000
+  name: 'twitter',
+  port: 61000
 })
 export class Twitter {
-    @Get("/1.1/search/tweets.json")
-    getTweets() {
-        return new JsonResponse({
-            "statuses": [
-                {
-                    "created_at": "Sun Feb 25 18:11:01 +0000 2018",
-                    "id": 967824267948773377,
-                    "id_str": "967824267948773377",
-                    "text": "From pilot to astronaut, Robert H. Lawrence was the first African-American to be selected as an astronaut by any na… https://t.co/FjPEWnh804",
-                    "truncated": true
-                }
-            ]
-        })
-    }
+  @Get('/1.1/search/tweets.json')
+  getTweets() {
+    return new JsonResponse({
+      'statuses': [
+        {
+          'created_at': 'Sun Feb 25 18:11:01 +0000 2018',
+          'id': 967824267948773377,
+          'id_str': '967824267948773377',
+          'text': 'From pilot to astronaut, Robert H. Lawrence was the first African-American to be selected as an astronaut by any na… https://t.co/FjPEWnh804',
+          'truncated': true
+        }
+      ]
+    })
+  }
 }
